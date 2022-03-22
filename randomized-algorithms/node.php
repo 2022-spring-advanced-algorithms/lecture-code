0188eff0ea88ac3b2c983ec41e49bdffa848da9f
Professor Steven Bitner <sbitner@uwf.edu>
2022-03-22 11:19:16 -0500

    Node copmplete

--- randomized-algorithms/node.php
+++ randomized-algorithms/node.php
@@ -3,14 +3,30 @@
 class Node
 {
 	private $data;
+	private $nexts = [];
 
 	function __construct ($inData)
 	{
 		$this->data = $inData;
 	}
 
-	public function getData()
+	public function setNext ($inData)
+	{
+		if (!is_null($this->nexts[$inData['level']]))
+		{
+			$inData['node']->setNext(['level' => $inData['level'], 'node' => $this->nexts[$inData['level']]]);
+		}
+
+		$this->nexts[$inData['level']] = $inData['node'];
+	}
+
+	public function getData ()
 	{
 		return $this->data;
+	}
+
+	public function getNext ($level)
+	{
+		return $this->nexts[$level];
 	}
 }
--- randomized-algorithms/test.php
+++ randomized-algorithms/test.php
@@ -10,5 +10,30 @@ class Test extends \PHPUnit\Framework\TestCase
 		$this->assertEquals(1, $node1->getData());
 		$node2 = new Node(2);
 		$node3 = new Node(3);
+
+		$node1->setNext(['node' => $node2, 'level' => 0]);
+
+		$this->assertEquals($node2, $node1->getNext(0));
+	}
+
+	public function testOtherLevels ()
+	{
+		$node1 = new Node(1);
+		$node2 = new Node(2);
+		$node3 = new Node(3);
+		$node1->setNext(['node' => $node2, 'level' => 0]);
+		$node2->setNext(['node' => $node3, 'level' => 0]);
+		$node1->setNext(['node' => $node3, 'level' => 1]);
+
+		$this->assertEquals($node2, $node1->getNext(0));
+		$this->assertEquals($node3, $node1->getNext(1));
+		$this->assertEquals($node3, $node2->getNext(0));
+
+		$node4 = new Node(4);
+		$node2->setNext(['node' => $node4, 'level' => 0]);
+
+		$this->assertEquals($node4, $node2->getNext(0));
+		$this->assertEquals($node3, $node4->getNext(0));
+
 	}
 }
